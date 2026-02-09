db62890928b497a529321b2cebdb75180322a992
lukebev06 <lmb73@kent.ac.uk>
2026-02-09 23:15:42 +0000

    3.2

--- part_3.py
+++ part_3.py
@@ -1,6 +1,17 @@
-speed = input("enter a speed between 0 and 100")
+speed = input("enter a speed between 0 and 100: ")
 
 try:
     print(f"Speed set to {speed}")
 except ValueError:
     print("Error: Corrupted Signal. Maintaining current speed.")
+
+def get_coordinate():
+    while True:
+        try:
+            x = int(input("what is the x coordinate: "))
+            print(x)
+            break
+        except ValueError:
+            print("Invalid coordinate")
+
+get_coordinate()
