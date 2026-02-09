cb1e57264336cbe72c3c5d8dba93c41cbeedacbb
lukebev06 <lmb73@kent.ac.uk>
2026-02-09 23:10:04 +0000

    2.3

--- part_2.py
+++ part_2.py
@@ -14,3 +14,10 @@ rover_status["Heater"] ="on"
 rover_status["Speed"] = 5
 
 print(rover_status)
+
+mission_log = [
+    {"Site": "Crater A", "Radiation": "Low", "Water": False},
+    {"Site": "Dune B", "Radiation": "High", "Water": True}
+]
+for i in range(len(mission_log)):
+    print(f"Site {mission_log[i]["Site"]} has {mission_log[i]["Radiation"]} radiation")
